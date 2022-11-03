85b66ebce2f0bba36a9cfc96d3d73a645ebb9c31
julien3800156 <julien.joyet@yahoo.fr>
2022-11-03 23:28:07 +0100

    Update class_Sab.py
    méthode affiche Chemin

--- class_Sab.py
+++ class_Sab.py
@@ -1,4 +1,4 @@
-class pos(object):
+class Pos(object):
     """Position dans l'espace de jeu"""
     def __init__(self,x,y):
         self.x=x
@@ -12,12 +12,22 @@ class Carte(object):
 
 class Chemin(Carte):
     """Carte Chemin"""
+    table=[('(   )'),('( | )'),('(---)'),('( x )'),('(-x )'),('( x-)'),('(-+ )'),('( +-)')]
     def __init__(self,pos,typ,up,mi,do):
         "up,mi et do determine le contenu des trois parties de la carte chemin"
-        table=[('(   )'),('( | )'),('(---)'),('( x )'),('(-x )'),('( x-'),('(-+ )'),('( +-)')]
         Carte.__init__(self,pos,typ)
         self.up=up
         self.mi=mi
         self.do=do
+
+    def affiche(self,x):
+        if x==0:
+            print(Chemin.table[self.up],end = "")
+        if x==1:
+            print(Chemin.table[self.mi],end = "")
+        if x==2:
+            print(Chemin.table[self.do],end = "")
+
+    
         
         
