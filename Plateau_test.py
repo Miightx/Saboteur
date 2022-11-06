bf64099717b832f77e53e5c454e6d0cbc84ba00a
julien3800156 <julien.joyet@yahoo.fr>
2022-11-06 16:10:21 +0100

    simplification fonction test plateau

--- Plateau_test.py
+++ Plateau_test.py
@@ -1,14 +1,12 @@
 import class_Sab as sb
 import numpy as np
 
-pos1=sb.Pos(0,0)
-pos2=sb.Pos(1,1)
 deck=[]
 
-carte=sb.Cheminrand(pos1,0)
-deck.append(carte)
-carte=sb.Cheminrand(pos2,0)
-deck.append(carte)
+
+deck.append(sb.Cheminrand(sb.Pos(4,2),0))
+
+deck.append(sb.Cheminrand(sb.Pos(1,1),0))
 
 plateau=[[sb.Casevide for x in range(9)] for x in range(5)]
 for i in range(0,len(deck)):
