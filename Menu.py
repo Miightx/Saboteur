ecefe0aff821e27486a3df0d661246d982185b0d
Miightx <117952621+Miightx@users.noreply.github.com>
2022-11-13 13:25:31 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,69 +1,102 @@
 import random
 import numpy as np
-class Menu():
+
+
+class Menu:
     import random
 
-    def aff_wel():
+    def aff_wel(self):  # Affichage du début
         print("+--------------------------------------------------------------------+")
         print("| Welcome to SabOOtters, where dwarf otters look for gold in a mine! |")
         print("+--------------------------------------------------------------------+")
 
-    def __init__(self):
-        self.number=0
-        self.personnage=[]
-        self.total_personnage=[]
-        self.joueur=[]
-        self.bot=[]
-        self.total=[]
-    def get_number(self):
+    def __init__(self):  # Property
+        self.number = 0
+        self.personnage = []
+        self.total_personnage = []
+        self.total_nom=[]
+        self.joueur = []
+        self.bot = []
+        self.total = []
+        self.total_bot = []
+
+    def get_number(self):  # Nombre de joueurs
         print("How many players?")
-        etat=False
-        while (etat==False):
-            self.number = int(input())
-            if (self.number >2 and self.number<11):
-                print("There is", self.number, "players")
-                etat=True
+
+        etat = False
+        while (etat == False):
+            self.number = input()
+            if (len(self.number)==1 or len(self.number)==2):
+                if (self.number.isdecimal()==True):
+                    self.number=int(self.number)
+                    if (self.number > 2 and self.number < 11):
+                        print("There is", self.number, "players")
+                        etat = True
+                    else:
+                        print("Please choose a number between 3 and 10")
+                else:
+                    print('Please write a correct number')
             else:
-                print("Please choose a number between 3 and 10")
-                etat=False
-    def players(self):
+                print('Please write a number')
+
+
+    def players(self):  # Configuration des joueurs
         for k in range(self.number):
-            print('Please enter the name of player',k+1,' and its status (IA: 0, Human: 1):')
+            print('Please enter the name of player', k + 1 )
             print('')
-            self.joueur=input()
-            self.total.append('self.joueur')
-            print('The name of the player is:',self.joueur[k])
-            print('Press 0 if the player is an IA and Press 1 if the player is an Human')
-            self.bot=int(input())
-    def cartes_roles(self):
-        if(self.number==3):
-            self.personnage=['S','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 3)
-        elif(self.number==4):
-            self.personnage=['S','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 4)
-        elif(self.number==5):
-            self.personnage=['S','S','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 5)
-        elif(self.number==6):
-            self.personnage=['S','S','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 6)
-        elif(self.number==7):
-            self.personnage=['S','S','S','C','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 7)
-        elif(self.number==8):
-            self.personnage=['S','S','S','C','C','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 8)
-        elif(self.number==9):
-            self.personnage=['S','S','S','C','C','C','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 9)
-        elif(self.number==10):
-            self.personnage=['S','S','S','S','C','C','C','C','C','C','C']
-            self.total_personnage=self.random.sample(self.personnage, 10)
+            self.joueur = input()
+            self.total_nom.append(self.joueur)
+            print('The name of the player is:', self.total_nom[k])
+            print('Please press 0 if the player is an IA and press 1 if the player is an Human')
+            etat = False
+            while (etat == False):
+                self.bot = input()
+                if (self.bot == '1'):
+                    self.total_bot.append("Human")
+                    etat = True
+
+                elif (self.bot == '0'):
+                    self.total_bot.append("IA")
+                    etat = True
+                else:
+                    print("Veuillez saisir 0 (IA) ou 1 (Human):")
+                    etat = False
+
+    def cartes_roles(self):  # Saboteur ou Mineur
+        if (self.number == 3):
+            self.personnage = ['S', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 3)
+        elif (self.number == 4):
+            self.personnage = ['S', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 4)
+        elif (self.number == 5):
+            self.personnage = ['S', 'S', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 5)
+        elif (self.number == 6):
+            self.personnage = ['S', 'S', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 6)
+        elif (self.number == 7):
+            self.personnage = ['S', 'S', 'S', 'C', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 7)
+        elif (self.number == 8):
+            self.personnage = ['S', 'S', 'S', 'C', 'C', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 8)
+        elif (self.number == 9):
+            self.personnage = ['S', 'S', 'S', 'C', 'C', 'C', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 9)
+        elif (self.number == 10):
+            self.personnage = ['S', 'S', 'S', 'S', 'C', 'C', 'C', 'C', 'C', 'C', 'C']
+            self.total_personnage = self.random.sample(self.personnage, 10)
             print(self.random.sample(self.personnage, 10))
 
-
-Menu.aff_wel()
+    def affichage_debut_fin(self):
+        for k in range (self.number):
+            self.total.append(self.total_nom[k] + ',(' + self.total_bot[k]+')')
+        self.total = ' '.join(self.total)
+        print('---------------------')
+        print('The', self.number, 'players are:', self.total)
 menu = Menu()
+menu.aff_wel()
 menu.get_number()
 menu.players()
+menu.affichage_debut_fin()
