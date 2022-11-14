5e432a26f404903e511c155f5b1595a3a6b7ee59
julien3800156 <julien.joyet@yahoo.fr>
2022-11-14 19:31:29 +0100

    Mise à jour class carte

--- classes/map.py
+++ classes/map.py
@@ -4,11 +4,15 @@ import numpy as np
 class Carte(object):
     """Carte du jeu SABOOTERS"""
     #Tables contenant le contenu des cartes
-    tablechemin=[('(   )'),('( | )'),('(---)'),('( x )'),('(-x )'),('( x-)'),('(-+ )'),('( +-)'),('(-+-)')]
+    tablechemin=[('(   )'),('( | )'),('(---)'),('( x )'),('(-x )'),('( x-)'),('(-+ )'),('( +-)'),('(-+-)'),('(-S-)'),('($$$)'),('(-N-)')]
     tableaction=[('(   )'),('(XXX)'),('(ATT)'),('(DEF)'),('( P )'),('( L )'),('( W )'),('( M )'),('(MAP)')]
+    tablerecto=[('(   )'),('(+++)'),('(+S+)'),('(END)')]
+    
+    
     #Matrices contenant contenant les vecteurs permettant d'atribuer une apparence a chaques types de cartes
-    matchemin=[[1,1,1],[0,2,0],[1,3,0],[0,3,1],[0,4,0],[0,5,0],[1,8,1],[1,6,1],[0,6,1],[1,6,0],[1,7,1],[0,7,1],[1,7,0],[1,8,1]]
+    matchemin=[[1,1,1],[0,2,0],[1,3,0],[0,3,1],[0,4,0],[0,5,0],[1,8,1],[1,6,1],[0,6,1],[1,6,0],[1,7,1],[0,7,1],[1,7,0],[1,8,1],[1,9,1],[10,10,10],[1,11,1]]
     mataction=[[1,1,1],[2,4,0],[2,5,0],[2,6,0],[2,4,5],[2,4,6],[2,5,6],[3,4,0],[3,5,0],[3,6,0],[3,4,5],[3,4,6],[3,5,6],[7,8,4]]
+    matrecto=[[1,2,1],[0,3,0]]
 
     def __init__(self,typ):
         #On defini une position par default
@@ -17,30 +21,59 @@ class Carte(object):
         self.__typ=typ
         #On defini un etat par default de la carte, etat neutre dans la pile de carte "0"
         self.__etat=0
+        #On defini par default que le carte est face cachee
+        self.__face=0
         #On tire au hasard une apparence a la carte selon son type
         if typ == 0 :
+            #Carte chemin
             self.__vectapparence=Carte.matchemin[np.random.choice(np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13]))]
+            self.__vectrecto=Carte.matrecto[0]
         if typ == 1 :
+            #Carte action
             self.__vectapparence=Carte.mataction[np.random.choice(np.array([0,1,2,3,4,5,6,7,8,9,10,11,12]))]
+            self.__vectrecto=Carte.matrecto[0]
         if typ == 2:
+            #Carte map
             self.__vectapparence=Carte.mataction[13]
+            self.__vectrecto=Carte.matrecto[0]
+        if typ == 3:
+            #Carte start
+            self.__vectapparence=Carte.matchemin[14]
+            self.__vectrecto=Carte.matrecto[0]
+        if typ == 4:
+            #Carte gold
+            self.__vectapparence=Carte.matchemin[15]
+            self.__vectrecto=Carte.matrecto[1]
+        if typ == 5:
+            #Carte pierre
+            self.__vectapparence=Carte.matchemin[16]
+            self.__vectrecto=Carte.matrecto[1]
 
     def affiche(self,x):
         #On affiche la partie de la carte que l'on souhaite afficher
-        if self.__typ == 0:
+        if self.__face==1:
+            if self.__typ == 0 or self.__typ == 3 or self.__typ == 4 or self.__typ == 5:
+                if x==0:
+                    print(Carte.tablechemin[self.__vectapparence[0]],end = "")
+                if x==1:
+                    print(Carte.tablechemin[self.__vectapparence[1]],end = "")
+                if x==2:
+                    print(Carte.tablechemin[self.__vectapparence[2]],end = "")
+            if self.__typ == 1 or self.__typ == 2:
+                if x==0:
+                    print(Carte.tableaction[self.__vectapparence[0]],end = "")
+                if x==1:
+                    print(Carte.tableaction[self.__vectapparence[1]],end = "")
+                if x==2:
+                    print(Carte.tableaction[self.__vectapparence[2]],end = "")
+        if self.__face==0:
             if x==0:
-                print(Carte.tablechemin[self.__vectapparence[0]],end = "")
+                print(Carte.tablerecto[self.__vectrecto[0]],end = "")
             if x==1:
-                print(Carte.tablechemin[self.__vectapparence[1]],end = "")
+                print(Carte.tablerecto[self.__vectrecto[1]],end = "")
             if x==2:
-                print(Carte.tablechemin[self.__vectapparence[2]],end = "")
-        if self.__typ == 1 or self.__typ == 2:
-            if x==0:
-                print(Carte.tableaction[self.__vectapparence[0]],end = "")
-            if x==1:
-                print(Carte.tableaction[self.__vectapparence[1]],end = "")
-            if x==2:
-                print(Carte.tableaction[self.__vectapparence[2]],end = "")
+                print(Carte.tablerecto[self.__vectrecto[2]],end = "")
+
     @property
     def pos(self) : return self.__pos
     @property
@@ -48,11 +81,22 @@ class Carte(object):
     @property
     def vectapparence(self) : return self.__vectapparence
     @property
+    def vectrecto(self) : return self.__vectrecto
+    @property
     def etat(self) : return self.__etat
+    @property
+    def face(self) : return self.__face
+
+    @face.setter
+    def face(self,face):
+        self.__face=0
+        if face>=0 and face<=1:
+            self.__face=face
     @etat.setter
+    #0:"Pile de carte" 1:"Posee sur le plateau" 2:"main de joueur" 3:"defausse" 
     def etat(self,etat):
         self.__etat=0
-        if etat>=0 and etat<=4 :
+        if etat>=0 and etat<=3 :
             self.__etat=etat
     @pos.setter
     def pos(self, pos) : 
--- test/Plateau_test.py
+++ test/Plateau_test.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0,"../PROJETPYTHON")
 import classes.map as sb
 import numpy as np
 import fonc_affichage as aff
@@ -11,7 +13,8 @@ deck[0].etat=1
 deck[1].etat=1
 deck[1].pos=[3,6]
 
-
+deck[0].face=1
+deck[1].face=1
 
 plateau=sb.Plateau()
 plateau.maj_cartes_posees(deck)
