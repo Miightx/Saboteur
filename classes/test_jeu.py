d62769fec407af08b6399d2d9352b940fd81dcc7
julien3800156 <julien.joyet@yahoo.fr>
2022-11-24 13:44:18 +0100

    fichier test

---
+++ classes/test_jeu.py
@@ -0,0 +1,9 @@
+from SABOOTTERSGAME import SABOOTERS
+
+
+
+jeu=SABOOTERS()
+jeu.initpartie()
+
+jeu.initmanche()
+jeu.tour_pour_rien()
--- test/test_jeu.py
+++ test/test_jeu.py
@@ -1,6 +1,13 @@
 import sys
 sys.path.insert(0,"../PROJETPYTHON")
-from classes import *
+from classes.deck import Deck
+from classes.menu import Menu
+from classes.player import Player
+from classes.hand import Hand
+
+from classes.mapp import Carte
+from classes.mapp import Plateau
+from classes.SABOOTTERSGAME import SABOOTERS
 
 
 
