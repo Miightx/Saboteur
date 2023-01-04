1a2f518a1a8a3abaf914054df55fa81b621ba5ad
Miightx <117952621+Miightx@users.noreply.github.com>
2023-01-04 02:14:50 +0100

    Un peu d'affichage

--- classes/menu.py
+++ classes/menu.py
@@ -3,10 +3,10 @@ import numpy as np
 import os
 
 
-
-
-
 class Menu(object):
+    """Allow to get information
+    about the game"""
+
     def __init__(self):  # Property
         self.__number = 0
         self.__players_name = []
@@ -16,19 +16,22 @@ class Menu(object):
         self.__sharing_gold = []
         self.__spm = []
         self.__total_score = []
-    #Methodes
+
+    # Methodes
     def __aff_wel(self):  # Affichage du début
+        """Show the welcome screen"""
         print("+--------------------------------------------------------------------+")
         print("| Welcome to SabOOtters, where dwarf otters look for gold in a mine! |")
         print("+--------------------------------------------------------------------+")
 
     def __get_number(self):  # Nombre de joueurs
+        """Put the number of players"""
         print("How many players?")
         etat = False
         while (etat == False):
             self.__number = input()
-            if (len(self.__number)==1 or len(self.__number)==2):
-                if (self.__number.isdecimal()==True):
+            if (len(self.__number) == 1 or len(self.__number) == 2):
+                if (self.__number.isdecimal() == True):
                     self.__number = int(self.__number)
                     if (self.__number > 2 and self.__number < 11):
                         print("There is", self.__number, "players")
@@ -40,12 +43,13 @@ class Menu(object):
             else:
                 print('Please write a number')
 
-
     def __players(self):  # Configuration des joueurs
+        """Define the list of player"""
         for k in range(self.__number):
-            os.system('cls' if os.name == 'nt' else 'clear')  #efface le contenue de la console, on verifie si on est sur windows ou pas
+            os.system(
+                'cls' if os.name == 'nt' else 'clear')  # efface le contenue de la console, on verifie si on est sur windows ou pas
             self.__aff_wel()
-            print('Please enter the name of player', k + 1 )
+            print('Please enter the name of player', k + 1)
             print('')
             joueur = 0
             joueur = input()
@@ -68,7 +72,8 @@ class Menu(object):
                     etat = False
 
     def __cartes_roles(self):  # Saboteur or Digger
-        personnage=[]
+        """Role of each player"""
+        personnage = []
         if (self.__number == 3):
             personnage = ['S', 'C', 'C', 'C']
             self.__roles = random.sample(personnage, 3)
@@ -95,23 +100,27 @@ class Menu(object):
             self.__roles = random.sample(personnage, 10)
 
     def __affichage_debut_fin(self):
-        total=[]
-        for k in range(self.__number-1):
-            total.append(self.__players_name[k] + '(' + self.__bot[k]+'),')
-        total.append(self.__players_name[self.__number-1] + '(' + self.__bot[self.__number-1]+')')
+        """Show the total number of players"""
+        total = []
+        for k in range(self.__number - 1):
+            total.append(self.__players_name[k] + '(' + self.__bot[k] + '),')
+        total.append(self.__players_name[self.__number - 1] + '(' + self.__bot[self.__number - 1] + ')')
         total = ' '.join(total)
         print('---------------------')
         print('The', self.__number, 'players are:', total)
 
     def start_game(self):
-        os.system('cls' if os.name == 'nt' else 'clear')  #efface le contenue de la console, on verifie si on est sur windows ou pas
+        """Used in SABOOTERS.py to get informations before the start"""
+        os.system(
+            'cls' if os.name == 'nt' else 'clear')  # efface le contenue de la console, on verifie si on est sur windows ou pas
         self.__aff_wel()
-        self.__get_number() 
-        self.__players() 
+        self.__get_number()
+        self.__players()
         self.__cartes_roles()
         self.__affichage_debut_fin()
 
     def change_role(self):
+        """For each set, change roles"""
         self.__cartes_roles()
 
     @property
@@ -129,48 +138,58 @@ class Menu(object):
     @property
     def roles(self):
         return self.__roles
+
     @property
     def count(self):
         return self.__count
+
     @count.setter
-    def count(self,count):
+    def count(self, count):
         self.__count = count
+
     @property
     def game_start(self):
         return self.__aff_wel(), self.__get_number(), self.__players(), self.__affichage_debut_fin()
+
     def count_winner(self):
+        """count the number of diggers and saboteurs"""
         self.__count = [0, 0]
-        for k in range(len(self.__roles)):   #Pour chaque roles je compte le nb de joueurs ayant ce role
-            if (self.__roles[k]=='S'):
+        for k in range(len(self.__roles)):  # Pour chaque roles je compte le nb de joueurs ayant ce role
+            if (self.__roles[k] == 'S'):
                 self.__count[0] += 1
-            elif (self.__roles[k]=='C'):
+            elif (self.__roles[k] == 'C'):
                 self.__count[1] += 1
         print(self.__count)
 
-    def winner(self,state,current_indice):
+    def winner(self, state, current_indice):
+        """state = 1 : Saboteurs won the set
+        Calculate golds (points) of each saboteur
+            state = 2: Digger won the set
+        Calculate golds (points) of each digger """
         self.state = state
         etat = False
         score_manche = np.zeros(self.__number)
         card_pull = 0
         self.__current_indice = current_indice
-        if (self.state == 1): #Disons que c'est le cas où les saboteurs ont gagnés == 1
+        if self.state == 1:  # Disons que c'est le cas où les saboteurs ont gagnés == 1
             print('Saboteurs won this game!')
             print('')
-            for k in range (self.__number):
-                if (self.__count[0] == 1):
-                    if (self.__roles[k] == 'S'):  #Un sabotteur, il obtient 4 pts
-                        score_manche[k]= 4
-                elif(self.__count[0] == 2 or self.__count[0] == 3): #2 ou 3 ils obtiennent 3 pts
-                    if (self.__roles[k] == 'S'):
+            for k in range(self.__number):
+                if (self.__count[0] == 1 and self.__roles[k] == 'S'):
+                    score_manche[k] = 4  # Un saboteur, il obtient 4 pts
+                elif (self.__count[0] == 2 or self.__count[0] == 3):  # 2 ou 3 ils obtiennent 3 pts
+                    if self.__roles[k] == 'S':
                         score_manche[k] = 3
-        elif (self.state == 2):     #Cas ou Mineurs gagnent == 2
+        elif (self.state == 2):  # Cas ou Mineurs gagnent == 2
             print('Diggers won this game!')
             self.__sharing_gold = np.random.randint(1, 4, size=self.__number)  # Carte d'or valant entre 1 et 3
             self.__sharing_gold = self.__sharing_gold.tolist()
             while (len(self.__sharing_gold) != 0):  # Jusqu'a quand y'a plus de pts a distribuer
                 if self.__current_indice == self.__number:
                     self.__current_indice = 0
-                if (self.__roles[self.__current_indice] == 'C'):  # Personnes étant Mineur                while (current_indice < self.count[1]):  # Addition des pts des mineurs quand il y a plus de cartes or que de mineurs
+                if (self.__roles[
+                    self.__current_indice] == 'C'):  # Personnes étant Mineur
+                    # while (current_indice < self.count[1]):  # Addition des pts des mineurs quand il y a plus de cartes or que de mineurs
                     print("It is the remaining gold cards")
                     print(self.__sharing_gold)
                     print(f"To {self.__players_name[self.__current_indice]} to choose the card he/she wishes")
@@ -183,8 +202,10 @@ class Menu(object):
                                 etat = True
                             else:
                                 print("Please choose another value")
-                    score_manche[self.__current_indice] += self.__sharing_gold[self.__choice - 1]  # Stockage des scores pour calcuer en fin de partie
-                    print(f"{self.__players_name[self.__current_indice]} choose {self.__sharing_gold[(self.__choice) - 1]}")
+                    score_manche[self.__current_indice] += self.__sharing_gold[
+                        self.__choice - 1]  # Stockage des scores pour calcuer en fin de partie
+                    print(
+                        f"{self.__players_name[self.__current_indice]} choose {self.__sharing_gold[(self.__choice) - 1]}")
                     del self.__sharing_gold[self.__choice - 1]  # Enlever la carte d'or choisit
                     self.__current_indice += 1
                     card_pull += 1
@@ -197,23 +218,27 @@ class Menu(object):
             print(self.__sharing_gold)
         print(f"The score is {score_manche}")
         self.__spm.append(score_manche)
-    def fin_de_manche(self,state,current_indice):
-        self.count_winner()
-        self.winner(state,current_indice)
-    def fin_de_partie(self):
 
+    def fin_de_manche(self, state, current_indice):
+        """Used in SABOOTTERS.py to get the score of each set"""
+        self.count_winner()
+        self.winner(state, current_indice)
+
+    def fin_de_partie(self):
+        """Used in SABOOTTERS.py to get the final score"""
         for i in range(self.__number):
             score_for_one_player = self.__spm[0][i] + self.__spm[1][i] + self.__spm[2][i]
             self.__total_score.append(score_for_one_player)
         print(f' Le score final est de {self.__total_score}')
+
     @property
     def total_score(self):
         return self.__total_score
 
-
     @property
     def spm(self):
         return self.__spm
+
     @property
     def current_indice(self):
         return self.__current_indice
