1cfe75d8777e3ec0d87e1a63c220b66cb42d3d70
julien3800156 <julien.joyet@yahoo.fr>
2023-01-06 22:46:06 +0100

    suppression de fichiers inutiles

--- Corbelle/tes_menu.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.insert(0,"../PROJETPYTHON")
-import classes.menu as sb
-state = 1
-menu=sb.Menu(1)
-menu.start_game()
--- Corbelle/test_jeu.py
+++
@@ -1,9 +0,0 @@
-from SABOOTTERSGAME import SABOOTERS
-
-
-
-jeu=SABOOTERS()
-jeu.initpartie()
-
-jeu.initmanche()
-jeu.tour_pour_rien()
